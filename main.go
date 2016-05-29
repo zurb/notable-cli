574a454142c3e763061bcaa2a6ae3ad032047319
Jordan Humphreys <jordan@mailyard.net>
2016-05-28 20:48:04 -0700

    Bump version.

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 var (
   platformHost = "https://notable.zurb.com"
-  version      = "0.1.0"
+  version      = "0.1.1"
 
   authPath      string
   s             = spinner.New(spinner.CharSets[6], 100*time.Millisecond)
